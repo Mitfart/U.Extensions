a8ad34e9b5e694c13e9cf7fa8888a0c6ce05a07a
Mitfart <60539376+Mitfart@users.noreply.github.com>
2023-05-21 16:49:41 +0400

    Add All Ext

---
+++ Runtime/TWithExt.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Extentions.Local {
+  public static class WithExt {
+    public static T With<T>(this T self, Action<T> apply) {
+      apply.Invoke(self);
+      return self;
+    }
+
+    public static T With<T>(this T self, Action<T> apply, bool       when) => when ? self.With(apply) : self;
+    public static T With<T>(this T self, Action<T> apply, Func<bool> when) => self.With(apply, when());
+  }
+}
