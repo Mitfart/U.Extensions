8b53cd2807f76714e4bab8d1d4abb34d6f983c27
Mitfart <60539376+Mitfart@users.noreply.github.com>
2023-05-21 16:50:10 +0400

    Add Layer Ext

---
+++ Runtime/Layer/EqualExt.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+namespace Extentions.Local {
+  public static class EqualExt {
+    public static bool Equal(this LayerMask mask, LayerMask layerMask) => (layerMask.value & (1 << mask)) > 0;
+  }
+}
