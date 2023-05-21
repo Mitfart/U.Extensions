fab2d8a863c73c9c341ca30a3590c142a74bd507
Mitfart <60539376+Mitfart@users.noreply.github.com>
2023-05-21 16:49:31 +0400

    Add Vector Ext

---
+++ Runtime/Vector/GetCirclePosExt.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Extentions.Local {
+  public static class GetCirclePosExt {
+    public static Vector2 GetCirclePos(
+      this Vector2 pos,
+      Vector2      center,
+      float        angle,
+      float        radius   = 1,
+      float        maxRange = 360
+    ) {
+      float ang = angle * maxRange / 360;
+      pos.x = center.x + radius * Mathf.Sin(ang * Mathf.Deg2Rad);
+      pos.y = center.y + radius * Mathf.Cos(ang * Mathf.Deg2Rad);
+      return pos;
+    }
+  }
+}
